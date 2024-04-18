b761c05ea2e5f3668114d1d7051f211656a507a1
ErnarKBTU <123051594+ErnarM04@users.noreply.github.com>
2024-04-18 22:44:56 +0500

    Added NavBar

--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {CommonModule, NgIf} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  loggedIn: boolean = true;
 
 }
