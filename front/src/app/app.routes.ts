d8e4c43bf2ad3236e920bd6cc163a359ee263c98
Anuza <a_akturina@kbtu.kz>
2024-04-23 13:29:47 +0600

    Add: Categories, Methods in Service, Routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'book/:id',component:BookDetailComponent}
 ];
