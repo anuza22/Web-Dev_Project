3cf533b98b932ee8a42c7856f2f55dd5dc8939d2
Anuza <a_akturina@kbtu.kz>
2024-04-25 22:12:43 +0600

    Add: Register.ts Del: User-Reg

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path:'book/:id',component:BookDetailComponent},
-    {path:'register', component:UserRegisterComponent}
+    {path:'register', component:RegisterComponent}
 ];
--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../service/register.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -8,5 +12,37 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  userData:FormGroup
+  constructor(private fb:FormBuilder,private regSer:RegisterService,private router:Router){
+    this.userData = this.fb.group({
+      username : ['',Validators.required],
+      email : ['',Validators.required],
+      password: ['',Validators.required],
+
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  userRegister(){
+
+    if(this.userData.valid){
+     const uservalue = this.userData.value
+     console.log(uservalue)
+ 
+     
+     this.regSer.userRegister(uservalue).subscribe((response)=>{
+       alert("Registered")
+       this.router.navigate(['login'])
+       console.log(uservalue)
+     })
+    }else{
+     alert("every field need valid data")
+    }
+   }
+ 
 
 }
